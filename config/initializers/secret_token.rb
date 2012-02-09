1df01747300f5e91387a6143de3c0566dbb4c54e
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2012-02-09 00:18:24 -0500

    New app to manage Indoor Rowing event

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CjrcIndoor::Application.config.secret_token = '5e912601ea5ba8e864de2afc1b631cc516fc9b07e824b9ea34e1847a03605ad023cf434bd9de2dd41edbebb744a2776e47dc544caf3c32105fede8623b575458'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CjrcIndoor::Application.config.session_store :cookie_store, key: '_cjrc_indoor_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CjrcIndoor::Application.config.session_store :active_record_store
